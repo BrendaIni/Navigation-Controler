e811e17964b71488eca67e835025b9125cb8c661
Brenda Iñiguez <brenda@Brendas-MacBook-Air.local>
2016-07-23 21:19:49 -0500

    Initial Commit

---
+++ Navigation Controller/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Navigation Controller
+//
+//  Created by Brenda Iñiguez on 7/23/16.
+//  Copyright (c) 2016 UAGMobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Navigation ControllerTests/Navigation_ControllerTests.m
@@ -0,0 +1,40 @@
+//
+//  Navigation_ControllerTests.m
+//  Navigation ControllerTests
+//
+//  Created by Brenda Iñiguez on 7/23/16.
+//  Copyright (c) 2016 UAGMobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Navigation_ControllerTests : XCTestCase
+
+@end
+
+@implementation Navigation_ControllerTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
