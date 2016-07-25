1c5ca4468234855deda61714ef851ab40129305f
Brenda Iñiguez <brenda@Brendas-MacBook-Air.local>
2016-07-24 21:24:16 -0500

    added table

---
+++ Navigation Controller/cellHome.h
@@ -0,0 +1,16 @@
+//
+//  CellsHomeTableViewCell.h
+//  Navigation Controller
+//
+//  Created by Brenda Iñiguez on 7/23/16.
+//  Copyright (c) 2016 UAGMobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface cellHome : UITableViewCell
+@property (strong, nonatomic) IBOutlet UIImageView *imgDestination;
+@property (strong, nonatomic) IBOutlet UILabel *lblDestinationTitle;
+@property (strong, nonatomic) IBOutlet UILabel *lblDescription;
+
+@end
---
+++ Navigation Controller/cellHome.m
@@ -0,0 +1,23 @@
+//
+//  CellsHomeTableViewCell.m
+//  Navigation Controller
+//
+//  Created by Brenda Iñiguez on 7/23/16.
+//  Copyright (c) 2016 UAGMobile. All rights reserved.
+//
+
+#import "cellHome.h"
+
+@implementation cellHome
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
